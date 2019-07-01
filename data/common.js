4e989975b772f413bef351d1c955ebbce08edaf3
phplego <phplego@gmail.com>
2019-07-01 03:15:43 +0300

    added data: html/js/style files

---
+++ data/common.js
@@ -0,0 +1,8 @@
+function insertMenu()
+{
+    document.write('Menu: ');
+    document.write('<a href="index.html">index.html</a> ');
+    document.write('<a href="log.html">log.html</a> ');
+    document.write('<a href="play.html">play.html</a> ');
+    document.write('<a href="settings.html">settings.html</a> ');
+}
